256b56501800890679566ba69e0e18edc2e52eed
Ricky-L21 <71520397+Ricky-L21@users.noreply.github.com>
2021-03-22 22:33:33 -0400

    Stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-console.log("Connected")
+console.log("Connected");
+
+import { questions } from "./questions";
+
+const startButton = document.getElementsById("start-btn");
+const questionContainerElement = document.getElementById("question-container");
+
+startButton.addEventListener("click", startGame);
+
+function startQuiz() {
+  console.log("Quiz go");
+  startButton.classlist.add("hide");
+  questionContainerElement.classList.remove("hide");
+  nextQuestion();
+}
+
+function nextQuestion() {}
+
+function selectAnswer() {}
---
+++ src/js/questions.js
@@ -0,0 +1,9 @@
+const questions = [
+    {
+        question: 'What is 2+2',
+        answer: [
+            {Text:'4', correct:true},
+            {Text:'11', correct:false},
+        ]
+    }
+]

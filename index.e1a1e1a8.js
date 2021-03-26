cf7bfa1e579e2f8f0090f087328c4c0b3cbd66df
Ricky-L21 <71520397+Ricky-L21@users.noreply.github.com>
2021-03-26 09:36:17 -0400

    ??????

---
+++ index.e1a1e1a8.js
@@ -0,0 +1,2 @@
+!function(){console.log("Connected");const t=document.getElementsById("start-btn");document.getElementById("question-container");t.addEventListener("click",startGame)}();
+//# sourceMappingURL=index.e1a1e1a8.js.map
--- src/js/questions.js
+++ src/js/questions.js
@@ -1,9 +1,15 @@
 const questions = [
-    {
-        question: 'What is 2+2',
-        answer: [
-            {Text:'4', correct:true},
-            {Text:'11', correct:false},
-        ]
-    }
-]
+  {
+    question: "What is 2+2",
+    answer: [
+      { Text: "4", correct: true },
+      { Text: "11", correct: false },
+    ],
+  },
+];
+
+//array for the questions
+
+//object
+
+//let score;
